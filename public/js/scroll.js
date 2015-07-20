5be93a3c711409b959eb747a5e5eb59fd4b94a34
James Didehvar <james@didehvar.com>
2015-07-20 23:41:16 +0100

    Wrap up design

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,17 +1,17 @@
-// $(document).ready(function() {
-//   /* http://stackoverflow.com/questions/23706003/changing-nav-bar-color-after-scrolling */
-//   var scroll_start = 0;
-//   var start_change = $('#change-navbar-text');
-//   var offset = start_change.offset();
-//
-//   if (start_change.length){
-//     $(document).scroll(function() {
-//       scroll_start = $(this).scrollTop();
-//       if(scroll_start > offset.top) {
-//         $('.header .navbar-default').css('background-color', 'rgba(0, 0, 0, 0.7)');
-//       } else {
-//         $('.header .navbar-default').css('background-color', 'transparent');
-//       }
-//     });
-//   }
-// });
+$(document).ready(function() {
+  /* http://stackoverflow.com/questions/23706003/changing-nav-bar-color-after-scrolling */
+  var scroll_start = 0;
+  var start_change = $('#change-navbar');
+  var offset = start_change.offset();
+
+  if (start_change.length){
+    $(document).scroll(function() {
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+        $('.header .navbar-default').css('background-color', 'rgba(0, 0, 0, 0.5)');
+      } else {
+        $('.header .navbar-default').css('background-color', 'transparent');
+      }
+    });
+  }
+});
